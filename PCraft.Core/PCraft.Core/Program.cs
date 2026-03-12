aaa69dbbc50ff2c54e2a2cccd0ccc34c964b5b2f
Gabriel Max Ferreira Rodrigues <gabriel.max@sga.pucminas.br>
2026-03-12 20:16:04 -0300

    Refatorado classe de conexao com o banco para utilizar ef core

--- PCraft.Core/PCraft.Core/Program.cs
+++ PCraft.Core/PCraft.Core/Program.cs
@@ -1,23 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 using PCraft.Core.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 app.MapDefaultControllerRoute();
 
-try
-{
-    using (var conn = Conexao.GetConexao())
-    {
-        conn.Open();
-        Console.WriteLine("conexao realizada!");
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine("erro na conexao: " + ex.Message);
-}
+
 
 app.Run();
